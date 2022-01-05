cf67a83bc3e1fd795dd67bcf4e588eb01b29b2f8
KevinAS28 <kevinagusto28@gmail.com>
2022-01-05 16:31:50 +0700

    added static directory

--- IOT0/settings.py
+++ IOT0/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SERVER_HOST = 'iot0.herokuapp.com'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
